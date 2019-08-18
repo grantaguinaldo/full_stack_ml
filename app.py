a937b4720e68293dc95da3749aa7befe8b34e1ce
GTA <grant.aguinaldo@gmail.com>
2019-08-17 17:26:14 -0700

    flask work

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from sklearn.linear_model import LogisticRegression
 from joblib import load
 import numpy as np
 
@@ -7,21 +8,38 @@ app = Flask(__name__)
 
 @app.route('/is_alive')
 def api_alive():
-    return 'I is alive!'
+    return 'I am alive!'
 
 
-@app.route('/api', methods=['GET'])
+@app.route('/prediction', methods=['GET'])
 def get_prediction():
-    # TODO: NEED TO GET DATA FROM THE GET REQUEST AND PASS IT INTO THE MODEL.
-    n = request.args.get('n')
 
-    input_data = np.array([])
+    sepal_length = request.args.get('sepallen')
+    sepal_width = request.args.get('sepalwid')
+    petal_length = request.args.get('petallen')
+    petal_width = request.args.get('petalwid')
+
+    input_list = [float(sepal_length), float(sepal_width), float(petal_length), float(petal_width)]
+    input_array = np.array(input_list).reshape(1, -1)
 
     pkl_lr = load('iris_class')
-    y_pred_class = pkl_lr(input_data)
+    y_pred_class = pkl_lr.predict(input_array).tolist()
+    y_pred_proba = pkl_lr.predict_proba(input_array).tolist()
 
-    return_package = {'iris_class': y_pred_class}
-    return jsonify(return_package)
+    # TODO: Need to figure out the datatype from y_pred_class and
+    #      y_pred_proba.
+
+    y_pred_package = dict(zip(['iris_class'], y_pred_class))
+
+    #TODO: NEED TO CREATE A DICT FROM
+
+    # y_pred_package = {'iris_class': y_pred_class[0]
+    #'iris_class_0_proba': y_pred_proba[0][0],
+    #'iris_class_1_proba': y_pred_proba[0][1],
+    #'iris_class_2_proba': y_pred_proba[0][2],
+    #}
+
+    return jsonify(y_pred_package)
 
 
 if __name__ == '__main__':
