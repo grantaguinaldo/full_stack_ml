7bc10410410fec5b1ec41d4c1c20e24105524f58
GTA <grant.aguinaldo@gmail.com>
2019-08-18 09:45:29 -0700

    API is working

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request, render_template
-from sklearn.linear_model import LogisticRegression
 from joblib import load
 import numpy as np
 
@@ -16,6 +15,20 @@ def main_page():
     return render_template('index.html')
 
 
+def class2string(x):
+    if x == np.array([0]):
+        return 'Setosa'
+    elif x == np.array([1]):
+        return 'Virginica'
+    elif x == np.array([2]):
+        return 'Versicolor'
+    else:
+        raise Exception('Based on the input values provided, unable to predict the Iris class of {}'.format(x))
+
+
+# TEST CASE 1: /prediction?sepallen=5.1&sepalwid=3.5&petallen=1.4&petalwid=5
+# TEST CASE 2: /prediction?sepallen=5.1&sepalwid=3.5&petallen=1.4&petalwid=0.2
+
 @app.route('/prediction', methods=['GET'])
 def get_prediction():
 
@@ -24,23 +37,28 @@ def get_prediction():
     petal_length = request.args.get('petallen')
     petal_width = request.args.get('petalwid')
 
-    input_list = [float(sepal_length), float(sepal_width), float(petal_length), float(petal_width)]
+    input_list = [float(sepal_length),
+                  float(sepal_width),
+                  float(petal_length),
+                  float(petal_width)]
+
     input_array = np.array(input_list).reshape(1, -1)
 
     pkl_lr = load('iris_class')
 
-    y_pred_class = pkl_lr.predict(input_array).tolist()
+    y_pred_class = pkl_lr.predict(input_array)
+    y_pred_prob = pkl_lr.predict_proba(input_array)
 
-    y_pred_class_dict = dict(zip(['iris_class'], y_pred_class))
+    y_pred_class_dict = dict(zip(['iris_class_name'],
+                                 [class2string(y_pred_class)]))
 
-    y_pred_proba = pkl_lr.predict_proba(input_array).tolist()
+    y_pred_class_prob_dict = dict(zip(['iris_class_setosa_prob',
+                                       'iris_class_virginica_prob',
+                                       'iris_class_versicolor_prob'], y_pred_prob[0].tolist()))
 
-    y_pred_class_prob_dict = dict(zip(['iris_class_prob'], y_pred_proba))
-
-    # Dict Unpacking: https://www.python.org/dev/peps/pep-0448/
-    y_pred_package = {**y_pred_class_dict, **y_pred_class_prob_dict}
-
-    return jsonify(y_pred_package)
+    # https://www.python.org/dev/peps/pep-0448/
+    # Only works on Python 3.5 and greater
+    return jsonify({**y_pred_class_dict, **y_pred_class_prob_dict})
 
 
 if __name__ == '__main__':
