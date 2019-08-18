85844b5e0c7661a3cd30ca7a5b8c53ff99132bc4
GTA <grant.aguinaldo@gmail.com>
2019-08-17 23:38:03 -0700

    Front End

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from sklearn.linear_model import LogisticRegression
 from joblib import load
 import numpy as np
@@ -9,6 +9,11 @@ app = Flask(__name__)
 @app.route('/is_alive')
 def api_alive():
     return 'I am alive!'
+
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
 
 
 @app.route('/prediction', methods=['GET'])
@@ -23,21 +28,17 @@ def get_prediction():
     input_array = np.array(input_list).reshape(1, -1)
 
     pkl_lr = load('iris_class')
+
     y_pred_class = pkl_lr.predict(input_array).tolist()
+
+    y_pred_class_dict = dict(zip(['iris_class'], y_pred_class))
+
     y_pred_proba = pkl_lr.predict_proba(input_array).tolist()
 
-    # TODO: Need to figure out the datatype from y_pred_class and
-    #      y_pred_proba.
+    y_pred_class_prob_dict = dict(zip(['iris_class_prob'], y_pred_proba))
 
-    y_pred_package = dict(zip(['iris_class'], y_pred_class))
-
-    #TODO: NEED TO CREATE A DICT FROM
-
-    # y_pred_package = {'iris_class': y_pred_class[0]
-    #'iris_class_0_proba': y_pred_proba[0][0],
-    #'iris_class_1_proba': y_pred_proba[0][1],
-    #'iris_class_2_proba': y_pred_proba[0][2],
-    #}
+    # Dict Unpacking: https://www.python.org/dev/peps/pep-0448/
+    y_pred_package = {**y_pred_class_dict, **y_pred_class_prob_dict}
 
     return jsonify(y_pred_package)
 
---
+++ test.py
@@ -0,0 +1,2 @@
+y_pred_proba_class = ['iris_class_' + str(i) + '_prob' for i in range(3)]
+print(y_pred_proba_class)
