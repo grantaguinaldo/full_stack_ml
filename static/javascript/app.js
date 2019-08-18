dc37c93c14fc0c1431e487b903a345468263de3e
GTA <grant.aguinaldo@gmail.com>
2019-08-18 15:08:14 -0700

    js work

---
+++ static/javascript/app.js
@@ -0,0 +1,5 @@
+var input $sepal_length = document.querySelector('_sepalLength');
+var input $sepal_width = document.querySelector('_sepalWidth');
+var input $petal_length = document.querySelector('_petalLength');
+var input $petal_width = document.querySelector('_petalWidth');
+
