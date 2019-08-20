1c41c7a5a3e29c6e6c7a5fced814dd7983619579
GTA <grant.aguinaldo@gmail.com>
2019-08-19 23:04:23 -0700

    Working on local machine. Ready for Heroku.

--- static/app.js
+++ static/app.js
@@ -1,7 +1,3 @@
-// Code works if placed directly into the HTML oage.
-// Problem linking .js file from within the Jinja template.
-console.log('Hi From the Top')
-
 // Select DOM elements and assign to variables.
 var $petalWidth = document.getElementById('_petalWidth');
 var $petalLength = document.getElementById('_petalLength');
@@ -10,50 +6,67 @@ var $sepalLength = document.getElementById('_sepalLength');
 var $searchBtn = document.getElementById('searchBtn');
 var $eraseBtn = document.getElementById('eraseBtn');
 var $json_response = document.getElementById('json_response');
+var $json_response_2 = document.getElementById('json_response_2');
+var $json_response_3 = document.getElementById('json_response_3');
+var $json_response_4 = document.getElementById('json_response_4');
 
 // Add event listeners and assign to functions.
 $searchBtn.addEventListener("click", predictClick);
 $eraseBtn.addEventListener("click", eraseData);
 
-
 // Build out functions to erase data from forms.
 function eraseData(){
 
+    // Clear out textboxes.
     $petalWidth.value = '';
     $petalLength.value = '';
     $sepalWidth.value = '';
     $sepalLength.value = '';
-    $json_response.value = '';
+
+    // Clear out innerHTML from DOM.
+    $json_response.innerHTML = '';
+    $json_response_2.innerHTML = '';
+    $json_response_3.innerHTML = '';
+    $json_response_4.innerHTML = '';
 };
 
 // Build out functions to take data in form and make prediction.
 function predictClick(){
 
+    // Get values from text boxes and store in variables.
     var petalWidthValue = $petalWidth.value;
     var petalLengthValue = $petalLength.value;
     var sepalWidthValue = $sepalWidth.value;
     var sepalLengthValue = $sepalLength.value;
-    var connectionString = 'prediction?sepallen='
+
+    // Build connection string from variables.
+    var connectionString = '/prediction?sepallen='
                             + sepalWidthValue + '&'
                             + 'sepalwid=' + sepalWidthValue + '&'
                             + 'petallen=' + petalLengthValue + '&'
                             + 'petalwid=' + petalWidthValue;
 
-    // console.log(connectionString);
-
-    Plotly.d3.json(connectionString, function(error, response) {
-      if (error) return console.warn(error);
-
-    console.log('Hi from the Event Listener Top');
-
-    var jsonObj = JSON.parse(response);
-
-    $json_response.innerHTML = jsonObj.iris_class_name;
-
-    // Contains JSON response.
-    console.log(jsonObj);
-
-    console.log('Hi from the Event Listener Bottom');
-
+    // Make API get request to the relevant end point.
+    fetch(connectionString)
+        .then(function(response) {
+            return response.json();
     });
+        .then(function(myJson) {
+
+    // Render the JSON response back to the DOM using innerHTML.
+    $json_response.innerHTML =  'Based on our machine learning model,\
+                                the predicted class of Iris is:' + ' '
+                                + myJson.iris_class_name + '.'
+
+    $json_response_2.innerHTML = 'The probability of the Setoasa class is' +
+                                 ' ' + myJson.iris_class_setosa_prob.toFixed(2) + '.' + ' '
+
+    $json_response_3.innerHTML = 'The probability of the Virginica class is' +
+                                ' ' + myJson.iris_class_virginica_prob.toFixed(2) + '.' + ' '
+
+    $json_response_4.innerHTML = 'The probability of the Versicolor class is' +
+                                ' ' + myJson.iris_class_versicolor_prob.toFixed(2) + '.'
+
+  });
+
 };
